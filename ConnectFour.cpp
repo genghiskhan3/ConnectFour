04c0badba35f20cca98ff868fff1f69a64856110
genghiskhan3 <mattleblanc333@gmail.com>
2015-03-18 00:31:31 -0400

    Update ConnectFour.cpp

--- ConnectFour.cpp
+++ ConnectFour.cpp
@@ -6,6 +6,8 @@ using namespace std;
 
 int main(){
 
+	// This code prompts the two users at the beginning of the game to enter their information
+	// Also checks to make sure none of the entered information will interfere with the design
 	char playerOneSymbol;
 	string playerOneName;
 	char playerTwoSymbol;
@@ -26,16 +28,21 @@ int main(){
 		else validInfoFields = true;
 	}
 
-	Player playerOne(playerOneSymbol, playerOneName);
-	Player playerTwo(playerTwoSymbol, playerTwoName);
-
-	Grid grid;
+	Player playerOne(playerOneSymbol, playerOneName);	// Initialize the first player
+	Player playerTwo(playerTwoSymbol, playerTwoName);	// Initialize the second player
+			
+	Grid grid;		// Initalize the grid
 
 	bool winner = false;
 	bool playerOnesTurn = true;
-	bool stale = false;
+	bool draw = false;
 
+
+	// This loop will run until there is a winner or the match ends in a draw
+	// The loop will continue to prompt the user for their move choice and manage the flow of the game
 	while (true){
+
+		// Variables to keep track of the current players name and symbol
 		char curSymbol;
 		string curName;
 		if (playerOnesTurn){
@@ -47,31 +54,40 @@ int main(){
 		}
 		
 		
-		int move;
-		bool valid = false;
+		string checkMove;		// Used to make sure the input is a valid integer and wont create an error
+		int move;				// If the input is an integer the string is converted to this int
+		bool valid = false;		// Bool used to flag invalid input and make sure valid input is entered
+
+		// Loop will run unitl a valid move is entered by the current user
 		while (!valid){
 			if (playerOnesTurn) playerOne.requestMove();
 			else playerTwo.requestMove();
 			
-
-			cin >> move;
-
-			valid = grid.validMove(move);
+			cin >> checkMove;
+			if (atoi(checkMove.c_str()) != 0){
+				move = atoi(checkMove.c_str());
+				valid = grid.validMove(move);
+			} else
+				valid = false;
 
 			if (!valid) cout << "\nSorry that is not a valid move, please enter a new move\n";
 			
 
 		}
 
+		// Inserts the players move into the grid
 		grid.insert(curSymbol, move);
 
+		// neatly displays the grid
 		cout << "\n\n\n\n";
 		grid.displayGrid();
 		cout << "\n\n";
 
-		stale = grid.stalemate();
-		if (stale) break;
+		// Checks every iteration to see if their is a draw
+		draw = grid.stalemate();
+		if (draw) break;
 
+		// Checks every iteration to see if their is a winner
 		winner = grid.checkForWinner(curSymbol);
 
 		if (winner) break;
@@ -80,7 +96,8 @@ int main(){
 		else playerOnesTurn = true;
 	}
 
-	if (playerOnesTurn && !stale) cout << "\n\nCONGRATULATIONS " << playerOne.getName() << " you are the winner" << endl;
-	else if(!playerOnesTurn && !stale) cout << "\n\nCONGRATULATIONS " << playerTwo.getName() << " you are the winner" << endl;
+	// Output endgame information to the users
+	if (playerOnesTurn && !draw) cout << "\n\nCONGRATULATIONS " << playerOne.getName() << " you are the winner" << endl;
+	else if(!playerOnesTurn && !draw) cout << "\n\nCONGRATULATIONS " << playerTwo.getName() << " you are the winner" << endl;
 	else cout << "\nThe match ends in a draw" << endl;
 }
